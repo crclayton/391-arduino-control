5a60d78c76407203e14dece9bdc7c599cea8808e
Charles Clayton <charlesrussellclayton@gmail.com>
2016-02-24 21:31:33 -0800

    Made variable names consistent and descriptive
    Since we're getting ready to add more PID control for the yaw, I labeled the altitude control as such.

--- controller/controller/controller.ino
+++ controller/controller/controller.ino
@@ -9,74 +9,73 @@
 #include <PID_v1.h>
 
 /* THESE NEED TO BE ADJUSTED UPON STARTUP :S */
-#define MIN_ANGLE_READING             427.0
-#define MAX_ANGLE_READING             638.0
+#define MIN_ALTITUDE_READING             427.0
+#define MAX_ALTITUDE_READING             638.0
 
 /*  The above readings are scaled between 0-255, and the
 angle setpoint is the desired altitude between 0-255, usually halfway  */
 #define ANGLE_SETPOINT                0.5 * 255 
 
-#define P   0.005  
-#define I   0.009
-#define D   0.005 // small because we have small resolution (200 samples)
+#define P_A   0.005  
+#define I_A   0.009
+#define D_A   0.005 // small because we have small resolution (200 samples)
 
-#define POTENTIOMETER_INPUT_PIN       3
-#define PID_OUTPUT_PIN                3  
-
-
-double pid_setpoint, pid_input, pid_output,
-potentiometer_input, resistor_input;
+#define ALTITUDE_INPUT_PIN       3
+#define ALTITUDE_OUTPUT_PIN           3  
 
 
-PID myPID(
-	&pid_input,     /* actual value */
-	&pid_output,    /* modification value, 0-255 */
-	&pid_setpoint,  /* desired value */
-	P, I, D, DIRECT);
+double pidAltitudeSetpoint,
+pidAltitudeInput,
+pidAltitudeOutput;	
+
+PID altitudePID(
+	&pidAltitudeInput,     /* actual value */
+	&pidAltitudeOutput,    /* modification value, 0-255 */
+	&pidAltitudeSetpoint,  /* desired value */
+	P_A, I_A, D_A, DIRECT);
 
 void setup()
 {
-	pinMode(PID_OUTPUT_PIN, OUTPUT);
-	pinMode(POTENTIOMETER_INPUT_PIN, INPUT);
+	pinMode(ALTITUDE_OUTPUT_PIN, OUTPUT);
+	pinMode(ALTITUDE_INPUT_PIN, INPUT);
 
 	Serial.begin(9600);
 
 	// 255   = 100% duty cycle, 1/490Hz    -> 2.041ms T - fastest
 	// 127.5 = 50%  duty cycle, 0.75/490Hz -> 1.531ms T - almost 0
-	myPID.SetOutputLimits(200, 250);
-	myPID.SetMode(AUTOMATIC);
+	altitudePID.SetOutputLimits(200, 250);
+	altitudePID.SetMode(AUTOMATIC);
 
 	/*set initial values*/
-	analogWrite(PID_OUTPUT_PIN, 200);
-	pid_setpoint = ANGLE_SETPOINT;
+	analogWrite(ALTITUDE_OUTPUT_PIN, 200);
+	pidAltitudeSetpoint = ANGLE_SETPOINT;
 }
 
 void loop()
 {
 	// setpoint driven w/ PID
-	double raw_input = analogRead(POTENTIOMETER_INPUT_PIN);
-
+	double raw_altitude = analogRead(ALTITUDE_INPUT_PIN);
 	// current altitude scaled between 0-255
-	pid_input = scaleBetween(raw_input, MIN_ANGLE_READING, MAX_ANGLE_READING, 0.0, 255.0);
+	pidAltitudeInput = scaleBetween(raw_altitude, MIN_ALTITUDE_READING, MAX_ALTITUDE_READING, 0.0, 255.0);
 
-	myPID.Compute();
-	analogWrite(PID_OUTPUT_PIN, pid_output);
+	altitudePID.Compute();
+	analogWrite(ALTITUDE_OUTPUT_PIN, pidAltitudeOutput);
 
 	Serial.print("Setpoint: ");
-	Serial.print(pid_setpoint);
+	Serial.print(pidAltitudeSetpoint);
 	Serial.print(",\tRaw Input:");
-	Serial.print(raw_input);
+	Serial.print(raw_altitude);
 	Serial.print(",\tScaled Input: ");
-	Serial.print(pid_input);
+	Serial.print(pidAltitudeInput);
 	Serial.print(",\tDiff: ");
-	Serial.print(percentDifference(pid_input, pid_setpoint));
+	Serial.print(percentDifference(pidAltitudeInput, pidAltitudeSetpoint));
 	Serial.print(",\tOutput: ");
-	Serial.print(pid_output);
+	Serial.print(pidAltitudeOutput);
 	Serial.print("\r\n");
 
-	// change altitude setpoint if value given
+	// change altitude setpoint if new value given
 	if (Serial.available() > 0) {
-		pid_setpoint = Serial.parseFloat();
+		pidAltitudeSetpoint = Serial.parseFloat();
 	}
 }
 
